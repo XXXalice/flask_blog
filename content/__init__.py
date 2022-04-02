0d308cb9172cb6bc4c6a0fe858f28cae08f44fb7
umemiya <euthanasia045@gmail.com>
2022-04-02 23:51:06 +0900

    config表示用path

--- content/__init__.py
+++ content/__init__.py
@@ -1,4 +1,6 @@
 import flask
+
 app = flask.Flask(__name__)
+app.config.from_object("content.config")
 
 import content.views
--- content/views.py
+++ content/views.py
@@ -1,5 +1,11 @@
 from content import app
 
+config_path = "/config/{}"
+
 @app.route("/")
 def main():
     return "<h1>test dayo</h1>"
+
+@app.route(config_path.format("debug"))
+def show_debug_status():
+    return "test"
